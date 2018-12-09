8deda56e9fe6f6d4e4323dd64f534de8320520ad
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-12-09 19:22:48 +0700

    edit MainActivity.java

--- app/src/main/java/com/example/gryphon/liquorfinder/MainActivity.java
+++ app/src/main/java/com/example/gryphon/liquorfinder/MainActivity.java
@@ -1,9 +1,28 @@
 package com.example.gryphon.liquorfinder;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.wonderkiln.camerakit.CameraView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CameraView cameraView;
+    Button btnDectect;
+    AlertDialog waitingDialog;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
